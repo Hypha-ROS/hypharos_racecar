19fa3a3a79a9d24013217cef4c1f0471276b77bd
Hypha ROS <hypha.ros@gmail.com>
2017-05-30 04:05:44 +0000

    update pkg

--- src/L1_controller.cpp
+++ src/L1_controller.cpp
@@ -1,3 +1,23 @@
+/*
+Copyright (c) 2017, ChanYuan KUO, YoRu LU,
+All rights reserved. (Hypha ROS Workshop)
+
+This file is part of hypha_racecar package.
+
+hypha_racecar is free software: you can redistribute it and/or modify
+it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published
+by the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+hypha_racecar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU LESSER GENERAL PUBLIC LICENSE for more details.
+
+You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+along with hypha_racecar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "ros/ros.h"
 #include <time.h>
 #include <geometry_msgs/PoseStamped.h>
