47796fd79fae009367cfc24aa7f9915b467cf761
Kai Wu <kai.wu013@gmail.com>
2017-10-16 17:45:30 +0800

    TEB parameter update/base control modify

--- script/base_control.py
+++ script/base_control.py
@@ -29,8 +29,7 @@ class BaseControl:
             self.rotat_z1 = 0.0
             self.flag=0
 	    self.count=0
-	    self.last_throttle=1500
-	    self.last_turn=90
+	    self.last_rotat_z=0
         except serial.serialutil.SerialException:
             rospy.logerr("Can not receive data from the port: "+device_port + 
             ". Did you specify the correct port in the launch file?")
@@ -45,20 +44,13 @@ class BaseControl:
 	self.rotat_z1 = data.angular.z
 	self.flag = data.linear.z
     def timerCmdCB(self, event):
+	if self.trans_x==0:
+		self.rotat_z=self.last_rotat_z
+
 	if self.flag==1:
 		self.trans_x=self.trans_x1
 		self.rotat_z=self.rotat_z1
-        # send cmd to motor
-        #if self.trans_x >0.05 and self.trans_x <=0.15 :
-	#	self.throttle = 1455
-	#elif self.trans_x >0.15 and self.trans_x <=0.4 :
-	#	self.throttle = 1450
-	#elif self.trans_x <-0.05 and self.trans_x >=-0.15 :
-	#	self.throttle = 1605
-	#elif self.trans_x <-0.15 and self.trans_x >=-0.4 :
-	#	self.throttle = 1615
-	#else:
-	#	self.throttle = 1550
+
 	if self.trans_x>0 and self.trans_x<1.5:
 		self.throttle = self.trans_x*-14+1456
 		self.turn = self.rotat_z*180/3.1415926*1.2+90
@@ -79,8 +71,7 @@ class BaseControl:
 	elif self.turn<45 :
 		self.turn=45
  
-        self.last_throttle=self.throttle
-	self.last_turn=self.turn  
+	self.last_rotat_z=self.rotat_z
         values = [str(self.turn), str(self.throttle), '0']
         values[2] = str(len(values[0]) + len(values[1]))
 	values1=[str(self.rotat_z), str(self.trans_x)]
